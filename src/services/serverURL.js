93e7d211cf92b153f7cd5b7d7f6c8e3aaa13f40e
Jeffry18 <jeffryrony14@gmail.com>
2025-09-16 15:44:31 +0530

    next23

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,3 +1,3 @@
-// const SERVER_URL ="https://taxibooking-server-2.onrender.com"
-const SERVER_URL ="http://localhost:5000"
+const SERVER_URL ="https://taxibooking-server-2.onrender.com"
+//const SERVER_URL ="http://localhost:5000"
 export default SERVER_URL
