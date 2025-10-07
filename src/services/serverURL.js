7b0ee33ee15f5d801d149c52d902138d791eac6e
Jeffry18 <jeffryrony14@gmail.com>
2025-10-07 11:50:17 +0530

    next44

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,3 +1,5 @@
-const SERVER_URL ="https://taxibooking-server-2.onrender.com"
+const SERVER_URL ="https://flymallu.com/api/"
+
+//const SERVER_URL ="https://taxibooking-server-2.onrender.com"
 //const SERVER_URL ="http://localhost:5000"
 export default SERVER_URL
