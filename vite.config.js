55b8c17201b7aad40d4b78add014caacd9fdaa63
Jeffry18 <jeffryrony14@gmail.com>
2025-10-17 19:11:54 +0530

    next59

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,22 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  
   server: {
     proxy: {
       '/api': {
-        target: 'https://flymallu.com/api',
-        //target: 'https://taxibooking-server-2.onrender.com',
-        //target: 'http://localhost:5000',
+        // target: 'https://flymallu.com/api',
+        target: 'https://taxibooking-server-2.onrender.com',
+        // target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false, // <-- ignore TLS cert issues locally
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     },
     logLevel: 'debug' // optional, helps debug proxy requests
+  },
+
+  build: {
+    chunkSizeWarningLimit: 2000 // increase warning limit to 2000 KB
   }
 })
